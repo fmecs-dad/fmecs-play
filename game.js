5cebeb2fcb36690c431538315267d16773b0464e
fmecs-dad <ybial.fmecs@gmail.com>
2026-02-02 22:52:13 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -325,7 +325,7 @@ const tutorialSteps = [
 
 async function fetchLeaderboard() {
   const response = await fetch(
-    "https://gjzqghhqpycbcwykxvgw.supabase.co/rest/v1/scores?select=score,duration_ms,undo_count,jokers_used,players(pseudo)&order=score.desc,duration_ms.asc,undo_count.asc,jokers_used.asc&limit=100",
+    "https://gjzqghhqpycbcwykxvgw.supabase.co/rest/v1/scores?select=score,duration_ms,undo_count,jokers_used,created_at,players(pseudo)&order=score.desc,duration_ms.asc,undo_count.asc,jokers_used.asc&limit=100",
     {
       headers: {
         "apikey": SUPABASE_ANON_KEY,
@@ -336,6 +336,7 @@ async function fetchLeaderboard() {
 
   return await response.json();
 }
+
 
 async function fetchPlayerScores(userId) {
   const response = await fetch(
