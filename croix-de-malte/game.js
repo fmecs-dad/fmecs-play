0d0723a80b84110abe2941789bf8b710e3ec5f76
fmecs-dad <ybial.fmecs@gmail.com>
2026-02-07 22:55:56 +0100

    Add files via upload

--- croix-de-malte/game.js
+++ croix-de-malte/game.js
@@ -263,7 +263,7 @@ function openHelpOverlay(auto = false) {
   const session = supa.auth.onAuthStateChange();
 
   if (session) {
-    console.log("
+    console.log("dans la boucle")
     await initialiserProfilEtLancerJeu(session);
   }
 
