8eda9eb56e815d71ec14ffca642ab962a83aff91
fmecs-dad <ybial.fmecs@gmail.com>
2026-02-09 18:21:46 +0100

    Add files via upload

--- croix-de-malte/game.js
+++ croix-de-malte/game.js
@@ -2367,7 +2367,7 @@ supa.auth.onAuthStateChange(async (event, session) => {
     const user = session?.user || null;
     //console.log("Utilisateur connecté :", user);
     await initialiserProfilEtLancerJeu(session);
-    updateAuthUI(session.user);
+    updateAuthUI(user);
     launchFlowOnce(user);
     return;
   }
